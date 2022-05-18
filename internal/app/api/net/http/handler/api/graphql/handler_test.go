81feed97c03d9f5a0e87510cf1e1f742d6ba313b
Ivan Lutokhin <ivan.lutokhin@gmail.com>
2022-05-18 21:10:04 +0300

    Remove beanstalk mocked pool

--- internal/app/api/net/http/handler/api/graphql/handler_test.go
+++ internal/app/api/net/http/handler/api/graphql/handler_test.go
@@ -1,6 +1,7 @@
 package graphql
 
 import (
+	"github.com/IvanLutokhin/go-beanstalk"
 	"github.com/IvanLutokhin/go-beanstalk-interface/internal/pkg/beanstalk/mock"
 	"net/http"
 	"net/http/httptest"
@@ -15,7 +16,12 @@ func TestHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	Handler(&mock.Pool{Client: &mock.Client{}}).ServeHTTP(recorder, request)
+	pool, err := beanstalk.NewPool(func() (beanstalk.Client, error) { return &mock.Client{}, nil }, 1, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	Handler(pool).ServeHTTP(recorder, request)
 
 	if code := recorder.Code; 200 != code {
 		t.Errorf("expected response status code '200', but got '%v'", code)
